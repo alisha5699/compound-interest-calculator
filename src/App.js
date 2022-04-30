e875f8f120e4b5e240e10ec0e24fc7e9ef23802e
alisha5699 <alishadhakal98@yahoo.com>
2022-04-30 08:39:24 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { useState } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import './index.css';
 
-function App() {
+
+const App = () => {
+  const[p, setP]=useState(0)
+  const[t, setT] = useState(0)
+  const[r, setR] = useState(0)
+  const[Ci, setCi] = useState(0)
+
+  const calculate = () =>{
+      setCi(p*(1+(r/100))^t)
+    }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <AppBar position="static">
+        <Toolbar>
+         
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Compound Interest Calculator
+          </Typography>
+
+    
+        </Toolbar>
+      </AppBar>
+
+      <br/><TextField onChange={(event)=>setP(event.target.value)} id="outlined-basic" label="Amount you want to deposit" variant="outlined" />
+      <br/><br/><TextField onChange={(event)=>setR(event.target.value)} id="outlined-basic" label="Rate" variant="outlined" />
+      <br/><br/><TextField onChange={(event)=>setT(event.target.value)} id="outlined-basic" label="Time" variant="outlined" />
+      <br/><br/><Button onClick={()=>calculate()} variant="contained">Calculate</Button>
+
+      <br/><p>Amount you will receive = {Ci} </p>
+     
     </div>
   );
 }
